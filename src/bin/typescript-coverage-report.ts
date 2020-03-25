c6643a93ceb92012fe5f6a6004366c1885fcf826
David Tsai <david@matternet.us>
2020-03-25 15:55:47 -0700

    fix: support typeCoverage atLeast option from user package.json

--- src/bin/typescript-coverage-report.ts
+++ src/bin/typescript-coverage-report.ts
@@ -1,15 +1,41 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+import * as fs from "fs";
+import * as path from "path";
 import generateCoverageReport from "../lib";
 import getOptions from "../lib/getOptions";
 
 const {
   version,
-  description,
-  typeCoverage = {}
+  description
   // eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require("../../package.json");
+
+/**
+ * Sets a default coverage threshold of `80`.
+ * If user provides `atLeast` in their `typeCoverage` configuration in
+ * their package.json, we set that as the program's default instead.
+ */
+const getAtLeast = () => {
+  let atLeast = 80;
+
+  const packageJsonPath = path.resolve(process.cwd(), "package.json");
+
+  if (fs.existsSync(packageJsonPath)) {
+    const currentPackageJson: {
+      typeCoverage?: {
+        atLeast?: number;
+      };
+    } = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+
+    if (currentPackageJson.typeCoverage?.atLeast) {
+      atLeast = currentPackageJson.typeCoverage.atLeast;
+    }
+  }
+
+  return atLeast;
+};
 
 const argvWithVersion = (argvs: string[]): string[] => {
   const vPos = argvs.indexOf("-v");
@@ -33,7 +59,7 @@ program
     "-t, --threshold [number]",
     "The minimum percentage of coverage required.",
     parseFloat,
-    typeCoverage.atLeast || 80
+    getAtLeast()
   )
   .option("-s, --strict [boolean]", "Run the check in strict mode.", false)
   .option("-d, --debug [boolean]", "Show debug information.", false)
