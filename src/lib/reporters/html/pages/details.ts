78f1b3fbbba60cc90daa52443ec51b51931dad1c
Stanyslas Bres <dzetah@pm.me>
2024-06-10 11:29:23 +0200

    chore: improve formatting of templated strings

--- src/lib/reporters/html/pages/details.ts
+++ src/lib/reporters/html/pages/details.ts
@@ -31,40 +31,40 @@ export const generateDetailsPage = ({
   const relativePathToIndex = path.relative(`${filename}.html`, "index.html");
 
   return `
-  <div style="margin-top: 3em;" class="ui container">
-    <h1 class="ui header">
-      <a href="${relativePathToIndex}">TypeScript coverage report</a>
-    </h1>
-    <table class="ui table celled fixed">
-      <thead>
-        <tr>
-          <th class="ten wide">Filename</th>
-          <th>Percent</th>
-          <th>Threshold</th>
-          <th>Total</th>
-          <th>Covered</th>
-          <th>Uncovered</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="${isValid ? "positive" : "negative"}">
-          <td>${filename}</td>
-          <td>${percentageCoverage}</td>
-          <td>${threshold}%</td>
-          <td>${totalCount}</td>
-          <td>${correctCount}</td>
-          <td>${totalCount - correctCount}</td>
-        </tr>
-      </tbody>
-    </table>
-    <textarea
-      id="editor"
-      readonly
-      style="margin-top: 3em;"
-    >${sourceCode}</textarea>
-    <pre id="annotations" style="display: none;">
-      ${JSON.stringify(annotations)}
-    </pre>
-  </div>
+    <div style="margin-top: 3em;" class="ui container">
+      <h1 class="ui header">
+        <a href="${relativePathToIndex}">TypeScript coverage report</a>
+      </h1>
+      <table class="ui table celled fixed">
+        <thead>
+          <tr>
+            <th class="ten wide">Filename</th>
+            <th>Percent</th>
+            <th>Threshold</th>
+            <th>Total</th>
+            <th>Covered</th>
+            <th>Uncovered</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="${isValid ? "positive" : "negative"}">
+            <td>${filename}</td>
+            <td>${percentageCoverage}</td>
+            <td>${threshold}%</td>
+            <td>${totalCount}</td>
+            <td>${correctCount}</td>
+            <td>${totalCount - correctCount}</td>
+          </tr>
+        </tbody>
+      </table>
+      <textarea
+        id="editor"
+        readonly
+        style="margin-top: 3em;"
+      >${sourceCode}</textarea>
+      <pre id="annotations" style="display: none;">
+        ${JSON.stringify(annotations)}
+      </pre>
+    </div>
   `;
 };
--- src/lib/reporters/html/pages/summary.ts
+++ src/lib/reporters/html/pages/summary.ts
@@ -1,8 +1,38 @@
 import path from "node:path";
-import { CoverageData } from "../../../getCoverage";
+import type { CoverageData } from "../../../getCoverage";
 
 type GenerateSummaryPageContext = Omit<CoverageData, "anys"> & {
   threshold: number;
+};
+
+const generateSummaryTableRow = ({
+  filename,
+  totalCount,
+  correctCount,
+  threshold
+}: {
+  filename: string;
+  correctCount: number;
+  totalCount: number;
+  threshold: number;
+}) => {
+  const percentage = totalCount === 0 ? 100 : (correctCount * 100) / totalCount;
+  const percentageCoverage = percentage.toFixed(2) + "%";
+  const isValid = percentage >= threshold;
+
+  const pathToFile = path.join("files", `${filename}.html`);
+
+  return `
+    <tr class="${isValid ? "positive" : "negative"}">
+      <td title="${filename}">
+        <a style="color: inherit;" href="${pathToFile}">${filename}</a>
+      </td>
+      <td>${percentageCoverage}</td>
+      <td>${totalCount}</td>
+      <td>${correctCount}</td>
+      <td>${totalCount - correctCount}</td>
+    </tr>
+  `;
 };
 
 export const generateSummaryPage = ({
@@ -16,63 +46,53 @@ export const generateSummaryPage = ({
   const isSummaryValid = percentage >= threshold;
 
   return `
-  <div style="margin-top: 3em;" class="ui container">
-  <h1 class="ui header">TypeScript coverage report</h1>
-  <h2 class="ui header">Summary</h2>
-  <table class="ui table celled fixed">
-    <thead>
-      <tr>
-        <th>Percent</th>
-        <th>Threshold</th>
-        <th>Total</th>
-        <th>Covered</th>
-        <th>Uncovered</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr class="${isSummaryValid ? "positive" : "negative"}">
-        <td>${percentage.toFixed(2) + "%"}</td>
-        <td>${threshold}%</td>
-        <td>${total}</td>
-        <td>${covered}</td>
-        <td>${uncovered}</td>
-      </tr>
-    </tbody>
-  </table>
-  <h2 class="ui header">Files</h2>
-  <table style="margin-top: 2em" id="coverage-table" class="ui table celled fixed sortable">
-    <thead>
-      <tr>
-        <th class="ten wide">Filename</th>
-        <th>Percent</th>
-        <th>Total</th>
-        <th>Covered</th>
-        <th>Uncovered</th>
-      </tr>
-    </thead>
-    <tbody>
-      ${Array.from(fileCounts)
-        .map(([filename, { correctCount, totalCount }]) => {
-          const percentage =
-            totalCount === 0 ? 100 : (correctCount * 100) / totalCount;
-          const percentageCoverage = percentage.toFixed(2) + "%";
-          const isValid = percentage >= threshold;
-
-          const pathToFile = path.join("files", `${filename}.html`);
-
-          return `
-          <tr class="${isValid ? "positive" : "negative"}">
-            <td title="${filename}">
-              <a style="color: inherit;" href="${pathToFile}">${filename}</a>
-            </td>
-            <td>${percentageCoverage}</td>
-            <td>${totalCount}</td>
-            <td>${correctCount}</td>
-            <td>${totalCount - correctCount}</td>
-          </tr>`;
-        })
-        .join("\n")}
-    </tbody>
-  </table>
-</section>`;
+    <div style="margin-top: 3em;" class="ui container">
+      <h1 class="ui header">TypeScript coverage report</h1>
+      <h2 class="ui header">Summary</h2>
+      <table class="ui table celled fixed">
+        <thead>
+          <tr>
+            <th>Percent</th>
+            <th>Threshold</th>
+            <th>Total</th>
+            <th>Covered</th>
+            <th>Uncovered</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="${isSummaryValid ? "positive" : "negative"}">
+            <td>${percentage.toFixed(2) + "%"}</td>
+            <td>${threshold}%</td>
+            <td>${total}</td>
+            <td>${covered}</td>
+            <td>${uncovered}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2 class="ui header">Files</h2>
+      <table style="margin-top: 2em" id="coverage-table" class="ui table celled fixed sortable">
+        <thead>
+          <tr>
+            <th class="ten wide">Filename</th>
+            <th>Percent</th>
+            <th>Total</th>
+            <th>Covered</th>
+            <th>Uncovered</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${Array.from(fileCounts)
+            .map(([filename, { correctCount, totalCount }]) =>
+              generateSummaryTableRow({
+                filename,
+                correctCount,
+                totalCount,
+                threshold
+              })
+            )
+            .join("\n")}
+        </tbody>
+      </table>
+    </div>
+  `;
 };
